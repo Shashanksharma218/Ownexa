54f99ee8b52ac1ca91ee4d5b6697066537cff0fd
Dhruv <dhruvnkejriwal@gmail.com>
2025-12-31 19:34:35 +0530

    Writing Contract

--- Contract/contracts/PropertyToken.sol
+++ Contract/contracts/PropertyToken.sol
@@ -2,24 +2,65 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PropertyToken is ERC1155, Ownable {
-
+    
     struct Property {
-        address owner;
         uint256 id;
         uint256 tokensupply;
-        uint256 propertyvalue; 
-        bool active; 
-    }  
+        string tokename;
+        uint256 pricepertoken;
+        bool active;
+    }
 
-     Property[] public allProperty ;
+    Property[] public allProperty;
 
-     
-    
+    mapping(uint256 => address) public propertyLister;
+
     constructor()
-        ERC1155("")           // metadata URI (can update later)
-        Ownable(msg.sender)   // initial owner
+        ERC1155("")
+        Ownable(msg.sender)
     {}
-}  
+
+    function listProperty(
+        uint256 _tokensupply,
+        uint256 _pricepertoken,
+        string memory _tokename
+    ) external {
+        require(_tokensupply > 0, "Invalid token supply");
+        require(_pricepertoken > 0, "Invalid price");
+
+        uint256 propertyId = allProperty.length;
+
+        allProperty.push(
+            Property({
+                id: propertyId,
+                tokensupply: _tokensupply,
+                tokename: _tokename,
+                pricepertoken: _pricepertoken,
+                active: true
+            })
+        );
+
+        propertyLister[propertyId] = msg.sender;
+
+        _mint(address(this), propertyId, _tokensupply, "");
+    }
+
+    function buyTokens(uint256 _propertyId, uint256 _amount) external payable {
+        require(_propertyId < allProperty.length, "Invalid property");
+        require(_amount > 0, "Invalid amount");
+
+        Property storage property = allProperty[_propertyId];
+        require(property.active, "Property not active");
+
+        uint256 available = balanceOf(address(this), _propertyId);
+        require(available >= _amount, "Not enough tokens left");
+
+        uint256 totalprice = property.pricepertoken * _amount;
+        require(msg.value == totalprice, "Incorrect ETH sent");
+
+        safeTransferFrom(address(this), msg.sender, _propertyId, _amount, "");
+    }
+}
